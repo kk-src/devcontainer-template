26abcf2258967bd129f87fb5aa3493457e587282
Krishnakumar R (KK) <65895020+kk-src@users.noreply.github.com>
2025-01-19 21:48:03 +0000

    Add python ext, cleanup Dockerfile & delete scripts

--- scripts/add-user.sh
+++
@@ -1,2 +0,0 @@
-useradd -ms /bin/bash pg-dev
-usermod -aG sudo pg-dev
--- scripts/install-dependencies.sh
+++
@@ -1,2 +0,0 @@
-apt-get update
-apt-get install -y build-essential libreadline-dev zlib1g-dev flex bison libxml2-dev libxslt-dev libssl-dev libxml2-utils xsltproc ccache meson libipc-run-perl gdb libc-dev vim
