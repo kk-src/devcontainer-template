fc60da9ffd572d7bf90215df130b09bb560b0c38
Krishnakumar R (KK) <65895020+kk-src@users.noreply.github.com>
2025-01-19 13:21:12 -0800

    Remove sudo

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,2 +1,2 @@
-sudo apt-get update
-sudo apt-get install -y build-essential libreadline-dev zlib1g-dev flex bison libxml2-dev libxslt-dev libssl-dev libxml2-utils xsltproc ccache meson libipc-run-perl gdb libc-dev vim
+apt-get update
+apt-get install -y build-essential libreadline-dev zlib1g-dev flex bison libxml2-dev libxslt-dev libssl-dev libxml2-utils xsltproc ccache meson libipc-run-perl gdb libc-dev vim
